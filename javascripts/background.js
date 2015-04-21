daabfe1cd14f95c656f1df6b6032eced18ef5951
Jonathan Chu <jmchuster@gmail.com>
2015-04-21 01:15:56 -0700

    remove old commented code that isn’t needed

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,10 +1,3 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- * @see http://developer.chrome.com/apps/app.window.html
- */
-
 $(function(){
   chrome.app.runtime.onLaunched.addListener(launchApp);
 
@@ -12,13 +5,8 @@ $(function(){
   var WIDTH = 400;
   var HEIGHT = 136;
 
-  // var scenarioResultHashId;
-  // var currentStep;
-
   chrome.runtime.onMessageExternal.addListener(
     function(request, sender, sendResponse) {
-      //if (sender.url == blacklistedWebsite)
-      //  return;  // don't allow this web page access
       if (request.launchApp) {
         var launchAppParams = {};
         chrome.storage.local.set(request.launchApp);
@@ -54,7 +42,7 @@ $(function(){
   window.model = new BackgroundModel(eventBus);
   window.controller = new BackgroundController(eventBus, model);
   window.websocket = new WebsocketController(eventBus, model, {
-    'websocketUrl': 'ws://localhost:5000/'
+    'websocketUrl': 'ws://video.asksonar.com/'
   });
   window.video = new VideoController(eventBus, model, {
     'canvas': document.getElementById('canvas'),
@@ -64,47 +52,3 @@ $(function(){
   window.audio = new AudioController(eventBus, model);
 
 });
-
-/*
-    if (msg.command == 'start') {
-      userScenarioUUID = msg.userScenarioUUID;
-      currentStep = msg.currentStep;
-
-      startVideoRecording(function() {
-        startAudioRecording(function(){
-          port.postMessage({'command': 'started'});
-        });
-      });
-    } else if (msg.command == 'next') {
-      userScenarioUUID = msg.userScenarioUUID;
-      currentStep = msg.currentStep;
-
-      stopAudioRecording(startAudioRecording);
-    } else if (msg.command == 'finish') {
-      userScenarioUUID = msg.userScenarioUUID;
-      currentStep = msg.currentStep;
-
-      stopAudioRecording(function(){
-        stopVideoRecording(function(){
-          port.postMessage({'command': 'stopped'});
-          userScenarioUUID = null;
-          currentStep = null;
-        });
-      });
-    } else if (msg.command == 'close') {
-      userScenarioUUID = msg.userScenarioUUID;
-      currentStep = msg.currentStep;
-
-      stopAudioRecording(function(){
-        stopVideoRecording(function(){
-          port.postMessage({'command': 'closed'});
-          userScenarioUUID = null;
-          currentStep = null;
-        });
-      });
-    } else {
-      console.log('Unrecognized command ' + msg.command);
-    }
-  });
-});
-*/
--- javascripts/popup/popup_controller.js
+++ javascripts/popup/popup_controller.js
@@ -54,37 +54,3 @@ PopupController.prototype.onScenarioAborted = function(event, step) {
     'step': step
   });
 }
-
-// PopupController.prototype.startAudio = function() {
-
-//   this.recognition = new webkitSpeechRecognition();
-//   this.recognition.continuous = true;
-//   this.recognition.interimResults = false;
-//   this.recognition.maxAlternatives = 1;
-//   this.recognition.onaudioend = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onaudioend'); }, this);
-//   this.recognition.onaudiostart = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onaudiostart'); }, this);
-//   this.recognition.onend = $.proxy(function() {
-//     console.log(this.currentAudioTime() + ': ' + 'onend');
-//     this.recognition.start();
-//   }, this);
-//   this.recognition.onerror = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onerror'); }, this);
-//   this.recognition.onnomatch = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onnomatch'); }, this);
-//   this.recognition.onresult = $.proxy(function(recognitionEvent) {
-//     console.log(this.currentAudioTime() + ': ' + 'onresult');
-//     console.log('onresult timestamp: ' + (recognitionEvent.timeStamp - this.recognitionStartTime));
-//     console.log('onresult text: ' + recognitionEvent.results[0][0].transcript);
-//   }, this);
-//   this.recognition.onsoundend = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onsoundend'); }, this);
-//   this.recognition.onsoundstart = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onsoundstart'); }, this);
-//   this.recognition.onspeechend = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onspeechend'); }, this);
-//   this.recognition.onspeechstart = $.proxy(function() { console.log(this.currentAudioTime() + ': ' + 'onspeechstart'); }, this);
-//   this.recognition.onstart = $.proxy(function() {
-//     this.recognitionStartTime = new Date().getTime();
-//     console.log(this.currentAudioTime() + ': ' + 'onstart');
-//   }, this);
-//   this.recognition.start();
-// }
-
-// PopupController.prototype.currentAudioTime = function() {
-//   return new Date().getTime() - this.recognitionStartTime;
-// }
