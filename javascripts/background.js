3297393d0560da131ea3c40a85796b637824f266
Jonathan Chu <jmchuster@gmail.com>
2015-05-06 12:47:20 -0700

    add test script

--- javascripts/background.js
+++ javascripts/background.js
@@ -23,6 +23,10 @@ $(function(){
   );
 
   function launchApp(scenarioResultHashId) {
+    if (typeof scenarioResultHashId != "string") {
+      return;
+    }
+
     currentWindow = chrome.app.window.create('popup.html?scenarioResultHashId=' + scenarioResultHashId, {
       id: "sonarDesktopCapture",
       frame: 'none',
@@ -57,5 +61,39 @@ $(function(){
     'encoderUrl': '/manifest_encoder.nmf',
     'fps': 10
   });
+
+  window.testLaunch = function() {
+    var testData = {
+      "YXjbMRzg":{
+        "hashid":"2mP50myq",
+        "description":"This a test of urls",
+        "steps":[
+          {
+            "hashid":"8jjenddw",
+            "description":"Bacon ipsum dolor amet pig consectetur irure ham, prosciutto tenderloin deserunt dolor. Elit strip steak pancetta, rump commodo andouille excepteur ut fugiat pork flank tongue tri-tip. Pork ipsum ut, cupim brisket turkey pork loin t-bone et cow ground round ribeye. Aliqua eiusmod enim doner brisket frankfurter eu kielbasa pork loin sunt officia bacon. Leberkas ball tip flank cupidatat pork chop meatloaf. Shankle enim laborum pariatur brisket irure.",
+            "url":"www.yourwebsite.com"
+          },
+          {
+            "hashid":"V3gW0ood",
+            "description":"Has http",
+            "url":"http://www.test.com/"
+          },
+          {
+            "hashid":"8D0ewPP2",
+            "description":"has https",
+            "url":"https://reddit.com"
+          },
+          {
+            "hashid":"V57O4oo5",
+            "description":"Has no site",
+            "url":""
+          }
+        ]
+      }
+    }
+
+    chrome.storage.local.set(testData);
+    launchApp(Object.keys(testData)[0]);
+  }
 
 });
