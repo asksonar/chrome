a0bf757732c075063be90ce617da19ce37a38205
Jonathan Chu <jmchuster@gmail.com>
2015-06-23 03:49:00 -0700

    whoops, missed adding the eventBus to Ajaxer defintion

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,9 +1,9 @@
 $(function(){
 
   window.eventBus = new BackgroundEventBus();
-  window.ajaxer = new Ajaxer({
-    'url': 'http://video.asksonar.com/api/v2/'
-    //'url': 'http://dockerhost:5000/api/v2/'
+  window.ajaxer = new Ajaxer(eventBus, {
+    // 'url': 'http://video.asksonar.com/api/v2/'
+    'url': 'http://dockerhost:5000/api/v2/'
   });
 
   window.model = new BackgroundModel(eventBus, ajaxer);
