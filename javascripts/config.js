fd3794af9266fb85fc6956f7e4822b024ac08738
Jonathan Chu <jmchuster@gmail.com>
2015-10-09 14:41:43 -0700

    move everything to app subfolder

--- javascripts/config.js
+++
@@ -1,7 +0,0 @@
-CONFIG = {
-  endpointUrl: 'http://my-local.asksonar.com',
-  s3UploadAccessKeyId: 'AKIAJPPESGO76I5HXLUQ',
-  s3UploadSecretAccessKey: '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',
-  s3UploadEndpoint: 'http://s3-us-west-1.amazonaws.com',
-  s3UploadBucket: 'upload.dev.engsonar.com'
-}
