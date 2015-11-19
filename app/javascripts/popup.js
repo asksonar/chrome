2cb0e85723fef713a79e1d2eb417b98defc3ae94
Jonathan Chu <jmchuster@gmail.com>
2015-11-19 15:05:18 -0800

    clean up button styling consistency

--- app/javascripts/popup.js
+++ app/javascripts/popup.js
@@ -24,7 +24,7 @@ $(function(){
     'section':      $('#div-start'),
     'btnFirstStep': $('#btn-first-step'),
     'width':        500,
-    'height':       300
+    'height':       290
   });
 
   var speechReminder = new SpeechReminder({
